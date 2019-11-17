a85a86fe4fee677423516525144b02b4c2c2d4af
andres castillo <andcastillo@gmail.com>
2019-11-16 22:53:13 -0500

    still failing on test 22

--- P1257D.java
+++ P1257D.java
@@ -5,30 +5,100 @@ public class P1257D {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
-		int i, j, n, m, lastMonster = 0, nDays = -1;
+		int i, j, k, n, m, power, endur, lastMonster = 0, prevMonster = 0, nDays = -1;
+		Hero hero;
+		int max;
 		int[] monsters = new int[200001];
-		int[][] heroes = new int[200001][2]; 
-		int[] endurance = new int[200001];
-		//int power = new int[200001];
 		for (i = 0; i < t; i++) {
 			n = in.nextInt();
+			max = Integer.MIN_VALUE;
 			for (j = 0; j < n; j++) {
 				monsters[j] = in.nextInt();
-				endurance[j] = 0;
+				if (monsters[j] > max)
+					max = monsters[j];
 			}
-			endurance[n] = 0;
 			m = in.nextInt();
+			ArrayList<Hero> heroesList = new ArrayList<Hero>();
 			for (j = 0; j < m; j++) {
-				heroes[j][0] = in.nextInt();
-				heroes[j][1] = in.nextInt();
-				endurance[heroes[j][1]] = 1;
+				power = in.nextInt();
+				endur = in.nextInt();
+
+				power = power > max ? max : power;
+				endur = endur > n ? n : endur;
+				//System.out.println(heroesList.size());
+				for (k = heroesList.size() - 1; k >= 0; k--) {
+					hero = heroesList.get(k);
+					if (power > hero.power && endur >= hero.endurance
+							|| power >= hero.power && endur > hero.endurance) {
+						heroesList.remove(k);
+					}
+					if ((power < hero.power && endur <= hero.endurance)
+							|| (power <= hero.power && endur < hero.endurance)
+							|| (power == hero.power && endur == hero.endurance)) {
+						power = -1;
+						endur = -1;
+						break;
+					}
+
+				}
+
+				if (power != -1 && endur != -1) {
+					heroesList.add(new Hero(power, endur));
+				} 
 			}
-
-			lastMonster = 0;
-			nDays = -1;
-
 			
+			nDays = -1;
+			// BFS
+			ArrayList<State> states = new ArrayList<State>();
+			State currentState;
+			states.add(new State(0, 0));
+			while (states.size() > 0) {
+				currentState = states.remove(0);
+				prevMonster = currentState.state;
+
+				if (prevMonster >= n) {
+					nDays = currentState.level;
+					break;
+				} else {
+					int bestMove = prevMonster;
+					for (k = heroesList.size() - 1; k >= 0; k--) {
+						lastMonster = prevMonster;
+						hero = heroesList.get(k);
+						int endurance = 1;
+						while (monsters[lastMonster] <= hero.power && hero.endurance >= endurance && lastMonster < n) {
+							endurance++;
+							lastMonster++;
+						}
+						if (lastMonster > bestMove) {
+							bestMove = lastMonster;
+						}
+					}
+					if (bestMove > prevMonster) {
+						states.add(new State(bestMove, currentState.level + 1));
+					}
+				}
+			}
+			System.out.println(nDays);
 		}
-		
+	}
+
+	static class State {
+		int level;
+		int state;
+
+		public State(int state, int level) {
+			this.level = level;
+			this.state = state;
+		}
+	}
+
+	static class Hero {
+		int power;
+		int endurance;
+
+		public Hero(int power, int endurance) {
+			this.power = power;
+			this.endurance = endurance;
+		}
 	}
 }
